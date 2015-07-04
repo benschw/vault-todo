14669e398a3e28a3ea7032a5daa94759b000184d
Ben Schwartz <benschw@gmail.com>
2015-07-03 23:36:52 -0500

    stuff

--- main.go
+++ main.go
@@ -12,17 +12,21 @@ func main() {
 	bind := flag.String("bind", "0.0.0.0:8080", "address to bind http server to")
 	flag.Parse()
 
+	log.Print("constructing service")
 	svc, err := todo.NewTodoService(*bind)
 	if err != nil {
 		log.Println(err)
 		os.Exit(1)
 	}
 
+	log.Print("migrating")
+
 	if err = svc.Migrate(); err != nil {
 		log.Println(err)
 		os.Exit(1)
 	}
 
+	log.Print("running service")
 	if err := svc.Run(); err != nil {
 		log.Println(err)
 		os.Exit(1)
